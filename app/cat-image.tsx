09268ec86a8ef70550c7558d72bc427152bd8302
Yuto Kaida <yutokaida@YutonoMacBook-Pro.local>
2025-09-10 14:05:31 +0900

    修正:cat-image.tsxの修正

--- app/cat-image.tsx
+++ app/cat-image.tsx
@@ -22,7 +22,7 @@ export function CatImage({ url }: CatImageProps) {
   return (
     <div className={styles.page}>
       <button onClick={refreshImage} className={styles.button}>
-        他のにゃんこも見る
+        One more cat!
       </button>
       <div className={styles.frame}>
         {imageUrl && <img src={imageUrl} className={styles.img} />}
